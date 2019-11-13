c38046b6e1a09d357d8080e357da60d3f3d9a56c
Michael Weedman <michael.weedman@gmail.com>
2019-11-13 09:43:02 -0700

    Working HelloWorld

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import HelloWorld from "./state-drills/HelloWorld";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
--- src/state-drills/HelloWorld.js
+++ src/state-drills/HelloWorld.js
@@ -9,6 +9,7 @@ export default class HelloWorld extends React.Component {
   }
 
   onClickHandler = e => {
+    console.log(e.target.value);
     this.setState({
       who: e.target.value
     });
@@ -18,9 +19,15 @@ export default class HelloWorld extends React.Component {
     return (
       <div>
         <p>Hello, {this.state.who}!</p>
-        <button onClick={this.onClickHandler}>Friend</button>
-        <button onClick={this.onClickHandler}>React</button>
-        <button onClick={this.onClickHandler}>World</button>
+        <button onClick={this.onClickHandler} value="Friend">
+          Friend
+        </button>
+        <button onClick={this.onClickHandler} value="React">
+          React
+        </button>
+        <button onClick={this.onClickHandler} value="World">
+          World
+        </button>
       </div>
     );
   }
