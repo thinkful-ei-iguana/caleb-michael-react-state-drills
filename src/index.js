703c5005435909b1eafe24e45c3742fecf0d9ebc
Michael Weedman <michael.weedman@gmail.com>
2019-11-13 15:45:17 -0700

    Updated testing

--- src/index.js
+++ src/index.js
@@ -5,18 +5,20 @@ import Accordion from "./state-drills/Accordion";
 
 const sectionsArray = [
   {
-    title: 'Section 1',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    title: "Section 1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
   },
   {
-    title: 'Section 2',
-    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    title: "Section 2",
+    content:
+      "Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!"
   },
   {
-    title: 'Section 3',
-    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-  },
-]
+    title: "Section 3",
+    content:
+      "Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?"
+  }
+];
 
 ReactDOM.render(
   <Accordion sections={sectionsArray} />,
