33bed04c4d4116b8de6a03c01a0916e79a974748
Caleb King <caleb.king536@gmail.com>
2019-11-13 14:30:13 -0700

    completed functionality for accordion drill

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import RouletteGun from "./state-drills/RouletteGun";
+import Accordion from "./state-drills/Accordion";
+
+const sectionsArray = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 ReactDOM.render(
-  <RouletteGun bulletInChamber={8} />,
+  <Accordion sections={sectionsArray} />,
   document.getElementById("root")
 );
---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastButton: null
+    };
+  }
+
+  onClickHandler = e => {
+    console.log(e.target.value);
+    this.setState({
+      lastButton: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.props.sections.map((section, index) => {
+          return (
+            <li key={index}>
+              <button onClick={this.onClickHandler} value={index}>{section.title}</button>
+              {(this.state.lastButton === `${index}`)  && <p>{section.content}</p>}
+            </li>
+          )
+        })
+        } 
+      </ul>
+    )
+  }
+}
+
+//
+
+// return (
+//   <div className='view'>
+//     {onStageParticipants.map(function(participant) {
+//       return (
+//       <div className='user'>
+//       <h3 className='name'>{participant.name}</h3>
+//       <img className='avatar' src={participant.avatar} alt="avatar"/> 
+//       </div>
+//       )      
+//   })}
+//   </div>
